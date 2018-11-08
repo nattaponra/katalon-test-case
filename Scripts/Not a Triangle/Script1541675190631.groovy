560eec707f6a337d0b40973585fec888313e06ca
Tee <Tee@10.202.218.17>
2018-11-08 18:07:14 +0700

    Not a Triangle

--- Scripts/Not a Triangle/Script1541675190631.groovy
+++ Scripts/Not a Triangle/Script1541675190631.groovy
@@ -13,3 +13,21 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://web.testing.se.nattaponra.com/')
+
+WebUI.setText(findTestObject('Object Repository/Test UI Triangle/Page_/input_ A_aSide'), '200')
+
+WebUI.setText(findTestObject('Object Repository/Test UI Triangle/Page_/input_ B_bSide'), '100')
+
+WebUI.setText(findTestObject('Object Repository/Test UI Triangle/Page_/input_ C_cSide'), '100')
+
+WebUI.click(findTestObject('Object Repository/Test UI Triangle/Page_/a_'))
+
+WebUI.click(findTestObject('Object Repository/Test UI Triangle/Page_/div_Not a Triangle'))
+
+WebUI.verifyElementText(findTestObject('Page_/div_Not a Triangle'), 'Not a Triangle')
+
+WebUI.closeBrowser()
+
