71de98fc0a9d6162b25bb1a11bc1ec7b8666e7de
nutzhiii <tanutchapanthum2018@gmail.com>
2018-11-08 19:25:48 +0700

    nut create api and web

---
+++ Test Suites/Nutto/API/APIReports.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>APIReports</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-08T19:22:13</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>32973f7e-84ea-47f5-81ae-a85a56319fd8</testSuiteGuid>
+   <testCaseLink>
+      <guid>65c2849b-bce5-46ed-9d2b-076204a051a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Nutto/API/TestCaseEqualivalent</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
